2e566bd5a48c1779908bb5336aa02fc7ced1b7e4
timilxr <38140090+timilxr@users.noreply.github.com>
2020-04-09 16:36:47 +0100

    1st challendge

--- src/estimator.js
+++ src/estimator.js
@@ -1,19 +1,27 @@
 const covid19ImpactEstimator = (data) => {
-    const input = data;
+    const impact = {};
+    const severeImpact = {};
+
+    switch (data.periodType) {
+        case 'weeks':
+            data.timeToElapse *= 7;
+            break;
+        case 'months':
+            data.timeToElapse *= 30;
+            break;
+    }
+    const days = data.timeToElapse;
+    const factor = Math.trunc(days / 3);
+
+    impact.currentlyInfected = data.reportedCases * 10;
+    severeImpact.currentlyInfected = data.reportedCases * 50;
+    impact.infectionsByRequestedTime = impact.currentlyInfected * (2 ** factor);
+    severeImpact.infectionsByRequestedTime = severeImpact.currentlyInfected * (2 ** factor);
     return {
-        data: input,
+        data,
         impact: {},
         severeImpact: {}
     };
 };
 
 export default covid19ImpactEstimator;
-
-// impact.currentlyInfected = covid19ImpactEstimator.reportedCases * 10;
-// severeImpact.currentlyInfected = reportedCases * 50;
-// infectionsByRequestedTime(impact.currentlyInfected);
-// infectionsByRequestedTime(severeImpact.currentlyInfected);
-
-// function infectionsByRequestedTime(x) {
-//     return x * (2 ** 10);
-// }
