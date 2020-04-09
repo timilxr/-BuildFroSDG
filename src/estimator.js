f62c4613df623879fe205825ba28a272d135417e
timilxr <38140090+timilxr@users.noreply.github.com>
2020-04-09 15:53:26 +0100

    first change to app

--- src/estimator.js
+++ src/estimator.js
@@ -1,3 +1,19 @@
-const covid19ImpactEstimator = (data) => data;
+const covid19ImpactEstimator = (data) => {
+    const input = data;
+    return {
+        data: input,
+        impact: {},
+        severeImpact: {}
+    };
+};
 
 export default covid19ImpactEstimator;
+
+// impact.currentlyInfected = covid19ImpactEstimator.reportedCases * 10;
+// severeImpact.currentlyInfected = reportedCases * 50;
+// infectionsByRequestedTime(impact.currentlyInfected);
+// infectionsByRequestedTime(severeImpact.currentlyInfected);
+
+// function infectionsByRequestedTime(x) {
+//     return x * (2 ** 10);
+// }
