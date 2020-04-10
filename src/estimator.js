b119bddb6c6633d15f087e7c0b2b140cb74186df
timilxr <38140090+timilxr@users.noreply.github.com>
2020-04-10 10:57:19 +0100

    retry first challendge

--- src/estimator.js
+++ src/estimator.js
@@ -1,27 +1,28 @@
 const covid19ImpactEstimator = (data) => {
     const impact = {};
     const severeImpact = {};
-
-    switch (data.periodType) {
-        case 'weeks':
-            data.timeToElapse *= 7;
-            break;
-        case 'months':
-            data.timeToElapse *= 30;
-            break;
+    const beds = (0.35 * data.totalHospitalBeds);
+    if (data.periodType === 'weeks') {
+        data.timeToElapse *= 7;
+    } else if (data.periodType === 'months') {
+        data.timeToElapse *= 30;
     }
     const days = data.timeToElapse;
     const factor = Math.trunc(days / 3);
-
     impact.currentlyInfected = data.reportedCases * 10;
     severeImpact.currentlyInfected = data.reportedCases * 50;
     impact.infectionsByRequestedTime = impact.currentlyInfected * (2 ** factor);
     severeImpact.infectionsByRequestedTime = severeImpact.currentlyInfected * (2 ** factor);
+    impact.severeCasesByRequestedTime = 0.15 * impact.infectionsByRequestedTime;
+    severeImpact.severeCasesByRequestedTime = 0.15 * severeImpact.infectionsByRequestedTime;
+
+    const severeCases = severeImpact.severeCasesByRequestedTime;
+    impact.hospitalBedsByRequestedTime = Math.ceil(beds - impact.severeCasesByRequestedTime);
+    severeImpact.hospitalBedsByRequestedTime = Math.ceil(beds - severeCases);
     return {
         data,
         impact: {},
         severeImpact: {}
     };
 };
-
 export default covid19ImpactEstimator;
