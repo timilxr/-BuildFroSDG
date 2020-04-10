2184f7c1d544a0dfb03b8ff420f03938446bd955
timilxr <38140090+timilxr@users.noreply.github.com>
2020-04-10 12:28:08 +0100

    last5

--- src/estimator.js
+++ src/estimator.js
@@ -1,41 +1,41 @@
 const covid19ImpactEstimator = (data) => {
-  const impact = {};
-  const severeImpact = {};
-  const beds = Math.trunc(0.35 * data.totalHospitalBeds);
-  const income = data.region.avgDailyIncomeInUSD;
-  const population = data.region.avgDailyIncomePopulation;
-  if (data.periodType === 'weeks') {
-    data.timeToElapse *= 7;
-  } else if (data.periodType === 'months') {
-    data.timeToElapse *= 30;
-  }
-  const days = data.timeToElapse;
-  const factor = Math.trunc(days / 3);
-  impact.currentlyInfected = data.reportedCases * 10;
-  severeImpact.currentlyInfected = data.reportedCases * 50;
-  impact.infectionsByRequestedTime = impact.currentlyInfected * (2 ** factor);
-  severeImpact.infectionsByRequestedTime = severeImpact.currentlyInfected * (2 ** factor);
-  impact.severeCasesByRequestedTime = 0.15 * impact.infectionsByRequestedTime;
-  severeImpact.severeCasesByRequestedTime = 0.15 * severeImpact.infectionsByRequestedTime;
+    const impact = {};
+    const severeImpact = {};
+    const beds = Math.trunc(0.35 * data.totalHospitalBeds);
+    const income = data.region.avgDailyIncomeInUSD;
+    const population = data.region.avgDailyIncomePopulation;
+    if (data.periodType === 'weeks') {
+        data.timeToElapse *= 7;
+    } else if (data.periodType === 'months') {
+        data.timeToElapse *= 30;
+    }
+    const days = data.timeToElapse;
+    const factor = Math.trunc(days / 3);
+    impact.currentlyInfected = data.reportedCases * 10;
+    severeImpact.currentlyInfected = data.reportedCases * 50;
+    impact.infectionsByRequestedTime = impact.currentlyInfected * (2 ** factor);
+    severeImpact.infectionsByRequestedTime = severeImpact.currentlyInfected * (2 ** factor);
+    impact.severeCasesByRequestedTime = 0.15 * impact.infectionsByRequestedTime;
+    severeImpact.severeCasesByRequestedTime = 0.15 * severeImpact.infectionsByRequestedTime;
 
-  const severeCases = severeImpact.severeCasesByRequestedTime;
-  impact.hospitalBedsByRequestedTime = Math.ceil(beds - impact.severeCasesByRequestedTime);
-  severeImpact.hospitalBedsByRequestedTime = Math.ceil(beds - severeCases);
+    const severeCases = severeImpact.severeCasesByRequestedTime;
+    impact.hospitalBedsByRequestedTime = Math.trunc(beds - impact.severeCasesByRequestedTime);
+    severeImpact.hospitalBedsByRequestedTime = Math.trunc(beds - severeCases);
 
-  impact.casesForICUByRequestedTime = Math.trunc(0.05 * impact.infectionsByRequestedTime);
-  severeImpact.casesForICUByRequestedTime = 0.05 * severeImpact.infectionsByRequestedTime;
+    impact.casesForICUByRequestedTime = Math.trunc(0.05 * impact.infectionsByRequestedTime);
+    severeImpact.casesForICUByRequestedTime = 0.05 * severeImpact.infectionsByRequestedTime;
 
-  impact.casesForVentilatorsByRequestedTime = Math.trunc(0.02 * impact.infectionsByRequestedTime);
-  severeImpact.casesForVentilatorsByRequestedTime = 0.02 * severeImpact.infectionsByRequestedTime;
+    impact.casesForVentilatorsByRequestedTime = Math.trunc(0.02 * impact.infectionsByRequestedTime);
+    severeImpact.casesForVentilatorsByRequestedTime = 0.02 * severeImpact.infectionsByRequestedTime;
 
-  const severeInfectionsByRequestedTime = severeImpact.infectionsByRequestedTime;
-  // eslint-disable-next-line max-len
-  impact.dollarsInFlight = Math.trunc(impact.infectionsByRequestedTime * income * population * days);
-  severeImpact.dollarsInFlight = severeInfectionsByRequestedTime * income * population * days;
-  return {
-    data,
-    impact,
-    severeImpact
-  };
+    const severeInfectionsByRequestedTime = severeImpact.infectionsByRequestedTime;
+    // eslint-disable-next-line max-len
+    impact.dollarsInFlight = Math.trunc(impact.infectionsByRequestedTime * income * population * days);
+    severeImpact.dollarsInFlight = severeInfectionsByRequestedTime * income * population * days;
+    return {
+        data,
+        impact,
+        severeImpact
+    };
 };
 export default covid19ImpactEstimator;
