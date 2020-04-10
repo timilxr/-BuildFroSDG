27c4d64f95aef5e89e1de6d27e1f947108c92d47
timilxr <38140090+timilxr@users.noreply.github.com>
2020-04-10 11:09:33 +0100

    update

--- src/estimator.js
+++ src/estimator.js
@@ -1,28 +1,28 @@
 const covid19ImpactEstimator = (data) => {
-    const impact = {};
-    const severeImpact = {};
-    const beds = (0.35 * data.totalHospitalBeds);
-    if (data.periodType === 'weeks') {
-        data.timeToElapse *= 7;
-    } else if (data.periodType === 'months') {
-        data.timeToElapse *= 30;
-    }
-    const days = data.timeToElapse;
-    const factor = Math.trunc(days / 3);
-    impact.currentlyInfected = data.reportedCases * 10;
-    severeImpact.currentlyInfected = data.reportedCases * 50;
-    impact.infectionsByRequestedTime = impact.currentlyInfected * (2 ** factor);
-    severeImpact.infectionsByRequestedTime = severeImpact.currentlyInfected * (2 ** factor);
-    impact.severeCasesByRequestedTime = 0.15 * impact.infectionsByRequestedTime;
-    severeImpact.severeCasesByRequestedTime = 0.15 * severeImpact.infectionsByRequestedTime;
+  const impact = {};
+  const severeImpact = {};
+  const beds = (0.35 * data.totalHospitalBeds);
+  if (data.periodType === 'weeks') {
+    data.timeToElapse *= 7;
+  } else if (data.periodType === 'months') {
+    data.timeToElapse *= 30;
+  }
+  const days = data.timeToElapse;
+  const factor = Math.trunc(days / 3);
+  impact.currentlyInfected = data.reportedCases * 10;
+  severeImpact.currentlyInfected = data.reportedCases * 50;
+  impact.infectionsByRequestedTime = impact.currentlyInfected * (2 ** factor);
+  severeImpact.infectionsByRequestedTime = severeImpact.currentlyInfected * (2 ** factor);
+  impact.severeCasesByRequestedTime = 0.15 * impact.infectionsByRequestedTime;
+  severeImpact.severeCasesByRequestedTime = 0.15 * severeImpact.infectionsByRequestedTime;
 
-    const severeCases = severeImpact.severeCasesByRequestedTime;
-    impact.hospitalBedsByRequestedTime = Math.ceil(beds - impact.severeCasesByRequestedTime);
-    severeImpact.hospitalBedsByRequestedTime = Math.ceil(beds - severeCases);
-    return {
-        data,
-        impact: {},
-        severeImpact: {}
-    };
+  const severeCases = severeImpact.severeCasesByRequestedTime;
+  impact.hospitalBedsByRequestedTime = Math.ceil(beds - impact.severeCasesByRequestedTime);
+  severeImpact.hospitalBedsByRequestedTime = Math.ceil(beds - severeCases);
+  return {
+    data,
+    impact: {},
+    severeImpact: {}
+  };
 };
 export default covid19ImpactEstimator;
