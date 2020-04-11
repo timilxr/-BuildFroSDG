ae89af440eb16ce860e4286d6f28ed68e04daee7
timilxr <38140090+timilxr@users.noreply.github.com>
2020-04-11 20:54:28 +0100

    last2

--- src/estimator.js
+++ src/estimator.js
@@ -31,8 +31,8 @@ const covid19ImpactEstimator = (data) => {
   severeImpact.casesForVentilatorsByRequestedTime = Math.floor(0.02 * severeImpactInfections);
 
   const severeInfectionsByRequestedTime = severeImpact.infectionsByRequestedTime;
-  const low = Math.floor((impact.infectionsByRequestedTime * income * population) / days);
-  impact.dollarsInFlight = low;
+  const lowe = Math.floor((impact.infectionsByRequestedTime * income * population) / days);
+  impact.dollarsInFlight = lowe;
   const sol = Math.floor((severeInfectionsByRequestedTime * income * population) / days);
   severeImpact.dollarsInFlight = sol;
   return {
